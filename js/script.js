e241a5cc80e31d958d6559a3246cf296c1395dc4
Lila Karpowicz <lila@karpowicz.dev>
2024-04-04 09:28:28 +0000

    display points plus refactor code

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // TODO: display points
     let points = 20;
     let numberToGuess = generateNumberToGuess();
     console.log(numberToGuess);
@@ -8,6 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     input.focus();
     const playButton = document.getElementById('play');
     const numberDisplay = document.getElementById('number');
+    const pointsDisplay = document.getElementById('points');
     // TODO: Implement keyboard and touchend events too
     playButton.addEventListener('click', () => {
         console.log("click!");
@@ -15,32 +17,42 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if (points <= 0) {
             // TODO: msg that player lost the game
-            // disable play button
+            // TODO: disable play button
             console.log("game over :(");
-        } else if (points > 0) {
-            if (guess >= 1 && guess <= 20) {
-                if (guess == numberToGuess) {
-                    // TODO: win!
-                    // disable input
-                    console.log("WIN!");
-                    playButton.disabled = true;
-                    numberDisplay.innerHTML = numberToGuess;
-                } else if (guess > numberToGuess) {
-                    points--;
-                    input.focus();
-                    // TODO: display message that number is lower
-                    console.log("number is lower");
-                } else if (guess < numberToGuess) {
-                    points--;
-                    input.focus();
-                    // TODO: Display msg that number is higher
-                    console.log('number is higher');
-                }
+        } else if (guess >= 1 && guess <= 20) {
+            if (guess == numberToGuess) {
+                // TODO: win!
+                // TODO: disable input
+                console.log("WIN!");
+                playButton.disabled = true;
+                input.disabled = true;
+                numberDisplay.innerHTML = numberToGuess;
             } else {
-                // TODO: Add error message
-                console.log("error: wrong value. Must be between 1 and 20");
+                points--;
+                pointsDisplay.innerHTML = points;
                 input.focus();
+                let message = 'number is lower';
+
+                if (guess < numberToGuess) {
+                    message = 'number is higher';
+                }
+
+                // if (guess > numberToGuess) {
+                //     // TODO: display message that number is lower
+                //     // TODO: display points
+                //     console.log("number is lower");
+                // } else if (guess < numberToGuess) {
+                //     // TODO: Display msg that number is higher
+                //     // TODO: display points
+                //     console.log('number is higher');
+                // }
+
+                // TODO: get message to display
             }
+        } else {
+            // TODO: Add error message
+            console.log("error: wrong value. Must be between 1 and 20");
+            input.focus();
         }
     });
     // TODO: reset button
